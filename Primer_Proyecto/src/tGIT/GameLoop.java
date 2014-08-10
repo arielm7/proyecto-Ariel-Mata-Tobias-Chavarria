9fc07c4205defa20c92df830be75869bbd99135a
Tobias Chavarria <tobias@Riemann>
2014-08-10 06:52:27 -0600

    New class WindowEvents (WindowListener)

--- Primer_Proyecto/src/tGIT/GameLoop.java
+++ Primer_Proyecto/src/tGIT/GameLoop.java
@@ -1,13 +1,69 @@
 package tGIT;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 
-public class GameLoop extends JFrame {
+public class GameLoop extends JFrame implements Runnable {
+	
+	int width = 600, heigth = 500;
+	boolean entrar;
+	Thread threadPrin;
 	
 	GameLoop(){
 		this.setTitle("Crazy Defender");
-		this.setBounds(200,200,600,500);
+		this.setBounds(200,200,width,heigth);
+		this.setResizable(false);
 		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.addWindowListener(new WindowEvents(this));
+		
+		
+		
+		
+		threadPrin = new Thread(this);
+		
+		threadPrin.start();
+		
+		
+		
+	}
+	
+	public void run() {
+		System.out.println("Started");
+		entrar=true;
+		while(entrar){
+			update();
+			render();
+			try {Thread.sleep(20);} 
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	private void update(){
+		
+	}
+	
+	private void render(){
+		Graphics g;
+		g = this.getGraphics();
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, width, heigth);
+		g.setColor(Color.black);
+		g.drawString("Crazy Defender", 200,100);
+		
+		Toolkit.getDefaultToolkit();
+		g.dispose();
 		
 		
 	}
@@ -16,6 +72,9 @@ public class GameLoop extends JFrame {
 		
 		GameLoop ventana= new GameLoop();
 	}
+
+
+	
 	
 
 }
---
+++ Primer_Proyecto/src/tGIT/WindowEvents.java
@@ -0,0 +1,62 @@
+package tGIT;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import tGIT.GameLoop;
+
+
+public class WindowEvents implements WindowListener {
+	
+	
+	GameLoop game;
+	
+	WindowEvents(GameLoop passgame){
+		this.game= passgame;
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
