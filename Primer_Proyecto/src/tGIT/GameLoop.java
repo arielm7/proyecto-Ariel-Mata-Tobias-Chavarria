8639fdd4e879989a5693c2e91fb7df9d680266e7
Tobias Chavarria <tobias@Riemann>
2014-08-25 04:40:29 -0600

    Mejora de la GUI

--- Primer_Proyecto/src/tGIT/GameLoop.java
+++ Primer_Proyecto/src/tGIT/GameLoop.java
@@ -21,14 +21,14 @@ public class GameLoop extends JFrame implements Runnable,KeyListener{
 	Thread threadPrin;
 	
 	private int direccion;    // Direccion del elefante
-	private int vidas;    // vidas del elefante
+	private int vidas,dx=5,dy=5;    // vidas del elefante
 	private int score;   // el puntaje de usuario.
 	private final int MIN = -5;    //Limite minimo al generar el numero random. 
 	private final int MAX = 6;    //Limite maximo al generar el numero random.
 	private static final int WIDTH = 1000;    //Ancho del JFrame
 	private static final int HEIGHT = 600;    //Alto del JFrame
-	private Image dbImage;	// Imagen a proyectar
-	private Image gameover;	// Imagen al finalizar el juego.
+	private Image dbImage,img_nave,img_nave2,img_nave3,img_nave4,img_nave5,img_nave6,img_nave7,img_nave8,img_nave9,img_nave10,img_nave11;	// Imagen a proyectar
+	private Image gameover,fondo;	// Imagen al finalizar el juego.
 	private Graphics dbg;	// Objeto grafico   //Arreglo del archivo divido.
 	private long tiempoActual;	//Tiempo de control de la animación
     private Ship nave1;
@@ -49,12 +49,34 @@ public GameLoop(){
 		int posY = (int) (Math.random() *(HEIGHT / 4));    // posicion en y es un cuarto del JFrame
 		
 		this.addKeyListener(this);
-		Image img_nave = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave.png"));
-		
+		//img_nave = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave.png"));
+		img_nave2 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave2.png"));
+		img_nave3 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave3.png"));
+		img_nave4 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave4.png"));
+		img_nave5 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave5.png"));
+		img_nave6 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave6.png"));
+		img_nave7 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave7.png"));
+		img_nave8 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave8.png"));
+		img_nave9 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave9.png"));
+		img_nave10 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave10.png"));
+		img_nave11 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/nave11.png"));
+	    fondo = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/tGIT/fondo.png"));
 		animaNave = new Animacion();
-		animaNave.sumaCuadro(img_nave, 100);
 		
-		nave1 = new Ship(posY, posY, img_nave);
+		//animaNave.sumaCuadro(img_nave, 100);
+		animaNave.sumaCuadro(img_nave2, 100);
+		animaNave.sumaCuadro(img_nave3, 100);
+		animaNave.sumaCuadro(img_nave4, 100);
+		animaNave.sumaCuadro(img_nave5, 100);
+		animaNave.sumaCuadro(img_nave6, 100);
+		animaNave.sumaCuadro(img_nave7, 100);
+		animaNave.sumaCuadro(img_nave8, 100);
+		animaNave.sumaCuadro(img_nave9, 100);
+		animaNave.sumaCuadro(img_nave10, 100);
+		animaNave.sumaCuadro(img_nave11, 100);
+		
+		
+		nave1 = new Ship(posY, posY, img_nave2);
 		threadPrin = new Thread(this);
 		threadPrin.start();
 		
@@ -98,24 +120,24 @@ public GameLoop(){
 		score += 5;  // el score se acumula en 5
 		switch(direccion){
 			case 1: { //se mueve hacia arriba con la flecha arriba.
-				nave1.setPosY(nave1.getPosY() - (11 - vidas) * 2);
+				nave1.setPosY(nave1.getPosY() - dy);
 				break;    	
 			}     
 			case 2: { //se mueve hacia abajo con la flecha abajo.
-				nave1.setPosY(nave1.getPosY() + (11 - vidas) * 2);
+				nave1.setPosY(nave1.getPosY() + dy);
 				break;    	
 			} 
 			case 3: { //se mueve hacia izquierda con la flecha izquierda.
-				nave1.setPosX(nave1.getPosX() - (11 - vidas) * 2);
+				nave1.setPosX(nave1.getPosX() - dx);
 				break;    	
 			}    
 			case 4: { //se mueve hacia derecha con la flecha derecha.
-				nave1.setPosX(nave1.getPosX() + (11 - vidas) * 2);
+				nave1.setPosX(nave1.getPosX() + dx);
 				break;    	
 			}	
 			case 5: { //se mueve hacia derecha con la flecha derecha.
-				nave1.setPosX(nave1.getPosX());
-				nave1.setPosY(nave1.getPosY());
+				nave1.setPosX(nave1.getPosX()+dx);
+				nave1.setPosY(nave1.getPosY()+dy);
 				break;    	
 			}		
 		}
@@ -153,13 +175,13 @@ public GameLoop(){
 	 */
     public void keyReleased(KeyEvent e){
     	if (e.getKeyCode() == KeyEvent.VK_UP) {    //Presiono flecha arriba
-			direccion = 2;
+			direccion = 5;
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {    //Presiono flecha abajo
-			direccion = 2;
+			direccion = 5;
 		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {    //Presiono flecha izquierda
-			direccion = 2;
+			direccion = 5;
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {    //Presiono flecha derecha
-			direccion = 2;
+			direccion = 5;
 		}
     	
     }
@@ -185,8 +207,10 @@ public void paint (Graphics g){
 	
 	public void paint1 (Graphics g){
     	if (vidas>0) {
-    		g.setColor(Color.yellow);
+    		g.setColor(Color.black);
     		g.fillRect(0, 0, WIDTH, HEIGHT);
+    		g.drawImage(fondo, 0,0, this);
+    		
     		if (nave1!=null) {
     			
 	    				g.drawImage(animaNave.getImagen(), nave1.getPosX(),nave1.getPosY(), this);
